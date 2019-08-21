eb0951fadedead68d7bd5012855dc39613f79314
david scheck <schecksdavid@gmail.com>
2019-08-21 17:21:53 +0200

    small fix

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{ cookiecutter.app_name }}/apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{ cookiecutter.app_name }}/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class {{ cookiecutter.model_name }}Config(AppConfig):
+class {{ cookiecutter.name_of_model }}Config(AppConfig):
     name = "{{ cookiecutter.project_slug }}.{{ cookiecutter.app_name }}"
     verbose_name = _("{{ cookiecutter.app_name }}")
 
